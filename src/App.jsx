f332ac5b7972bca5c74f3f7dd33f31f47cad71e6
buyercompassblog-beep <buyercompassblog@gmail.com>
2025-11-24 12:10:45 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import Snowfall from "./components/Snowfall";
+
 
 import HomePage from "./pages/HomePage";
 import ArticlePage from "./pages/ArticlePage";
